6f9ea53790470d925b25275fd87422f4c9c60e21
Max Weisser <maxweisser3@gmail.com>
2023-05-08 19:23:13 +0200

    Coding challange 2

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#ifndef __ESPL_LIB_H__
+#define __ESPL_LIB_H__
+
+char* num_to_words(unsigned int num);
+
+#endif
 
 int main(int argc, char* argv[])
 {
-    printf("Hello ESPL");
+    int y = 1;
+    char* n;
+    int i;
+
+    while(y == 1)
+    {
+        printf("Please enter a number: \n");
+        scanf("%d",&i);
+        n = num_to_words(i);
+        printf("Your Letter is: ");
+        printf("%c",*n);
+        printf("\n");
+        printf("Do you want to enter a new number?(enter 1 for yes or 0 for no) \n");
+        scanf("%d", &y);
+
+    }
+    
+
 
     return 0;
 }
